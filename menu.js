400543af3e1ef8e6409e484de64e8bff391d5188
florescobarlevi <florenciaescobarlevi@gmail.com>
2023-02-24 16:31:34 -0300

    agrege colores a prioridades tambien solucione el dropdown, falta darle el comportamiento a eso y a las prioridades tmb  y agregar los iconos q estan listos o sea TODO JS

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
 const add = document.getElementById('add')
 const popup_container = document.getElementById('popup-container')
 const close = document.getElementById('close')
+const cancel = document.getElementById('cancel')
 
 add.addEventListener('click', function(){
     popup_container.classList.add('show');
@@ -9,15 +10,40 @@ add.addEventListener('click', function(){
 close.addEventListener('click', function(){
     popup_container.classList.remove('show')
 })
+/*BOTON CANCELAR */
+cancel.addEventListener('click', function(){
+    let tareaIngresada = nuevaTarea.value 
+    nuevaTarea.value = ''
+    let descripcionIngresada = descripcion.value
+    descripcion.value = ''
+    let info = `<ol class="list-group list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                            <div class="fw-bold">${tareaIngresada}</div>
+                            ${descripcionIngresada}
+                        </div>
+                    </li>
+                </ol>`     
+    popup_container.classList.remove('show')
+})
 
-
+/*BOTON AGREGAR*/ 
 let padre = document.getElementById('listado')
 let nuevaTarea = document.getElementById('nuevaTarea')
+let descripcion = document.getElementById('descripcion')
 
 let addBtn = document.getElementById('close').addEventListener('click', function(){
     let tareaIngresada = nuevaTarea.value 
     nuevaTarea.value = ''
-    let info = `<li class="list-group-item">${tareaIngresada}</li>`
-    
-    padre.innerHTML += info         
+    let descripcionIngresada = descripcion.value
+    descripcion.value = ''
+    let info = `<ol class="list-group list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                            <div class="fw-bold">${tareaIngresada}</div>
+                            ${descripcionIngresada}
+                        </div>
+                    </li>
+                </ol>`    
+    padre.innerHTML += info       
 })
