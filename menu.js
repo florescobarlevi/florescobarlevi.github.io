6ad5f30eed0ef0442346dd3be099a05503eb61c8
florescobarlevi <florenciaescobarlevi@gmail.com>
2023-02-27 19:07:33 -0300

    agrege los iconos, pero cuando los clikeas se borra todo. chequear JS

--- menu.js
+++ menu.js
@@ -3,6 +3,8 @@ const popup_container = document.getElementById('popup-container')
 const close = document.getElementById('close')
 const cancel = document.getElementById('cancel')
 
+
+
 add.addEventListener('click', function(){
     popup_container.classList.add('show');
 })
@@ -10,22 +12,7 @@ add.addEventListener('click', function(){
 close.addEventListener('click', function(){
     popup_container.classList.remove('show')
 })
-/*BOTON CANCELAR */
-cancel.addEventListener('click', function(){
-    let tareaIngresada = nuevaTarea.value 
-    nuevaTarea.value = ''
-    let descripcionIngresada = descripcion.value
-    descripcion.value = ''
-    let info = `<ol class="list-group list-group">
-                    <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <div class="ms-2 me-auto">
-                            <div class="fw-bold">${tareaIngresada}</div>
-                            ${descripcionIngresada}
-                        </div>
-                    </li>
-                </ol>`     
-    popup_container.classList.remove('show')
-})
+
 
 /*BOTON AGREGAR*/ 
 let padre = document.getElementById('listado')
@@ -37,13 +24,23 @@ let addBtn = document.getElementById('close').addEventListener('click', function
     nuevaTarea.value = ''
     let descripcionIngresada = descripcion.value
     descripcion.value = ''
-    let info = `<ol class="list-group list-group">
+    let info = `<ol class="list-group list-group" >
                     <li class="list-group-item d-flex justify-content-between align-items-start">
                         <div class="ms-2 me-auto">
                             <div class="fw-bold">${tareaIngresada}</div>
                             ${descripcionIngresada}
                         </div>
+                        <div class="div-img">
+                            <a href=""><img src="/img/cheque.png"  class="img-list" alt=""></a>
+                            <a href=""><img src="/img/bote-de-basura.png"  class="img-list"  alt=""></a>
+                        </div>                                    
                     </li>
                 </ol>`    
     padre.innerHTML += info       
+})
+/*BOTON CANCELAR */
+cancel.addEventListener('click', function(){
+    nuevaTarea.value = ''
+    descripcion.value = ''    
+    popup_container.classList.remove('show')
 })
